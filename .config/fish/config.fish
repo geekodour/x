c75f7bbc8160d695e217d2b80bb9bf96fbf783be
Hrishikesh Barman <hrishikeshbman@gmail.com>
2022-05-19 21:53:01 +0530

    adding fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,17 @@
+if status is-interactive
+  # see https://www.funtoo.org/Keychain
+  keychain --eval --quiet --quick --nogui id_ed25519 | source
+end
+
+# sway poop asl;kjdlask
+if test (tty) = "/dev/tty1"
+  sway
+end
+
+# exports
+set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
+source ~/.config/nnn/init # nnn
+source ~/.config/cheat/init # cheat
+source ~/.config/starship/init # starship
+source /opt/asdf-vm/asdf.fish # asdf
