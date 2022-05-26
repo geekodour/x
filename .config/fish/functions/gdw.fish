c24a51462a71c3c7e415eaeb35c5d50cb7dd52de
Hrishikesh Barman <hrishikeshbman@gmail.com>
2022-05-26 14:44:04 +0530

    gallery and ytdl

---
+++ .config/fish/functions/gdw.fish
@@ -0,0 +1,3 @@
+function gdw --wraps=gallery-dl --description 'alias gdw gallery-dl'
+  gallery-dl $argv; 
+end
---
+++ .config/fish/functions/ytdw.fish
@@ -0,0 +1,3 @@
+function ytdw --wraps=yt-dlp --description 'alias ytdw yt-dlp'
+  yt-dlp $argv; 
+end
