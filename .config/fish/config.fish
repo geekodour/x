c304c9519ab78133bc695aa064f55b5d70256407
Hrishikesh Barman <hrishikeshbman@gmail.com>
2023-04-15 06:02:47 +0530

    updated stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,6 +15,16 @@ set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 set -x DOOMDIR "~/.config/doom"
 set -x EDITOR nvim
 
+# NOTE: I am not sure if these are necessary when we use asdf
+# NOTE: deal w npm beleg ke.
+set -x CARGO_HOME $XDG_DATA_HOME/cargo
+set -x GEM_HOME $XDG_DATA_HOME/gem
+set -x GOPATH $XDG_DATA_HOME/go # what aboout someone saying no need to set gopath
+set -x NIMBLE_DIR $XDG_DATA_HOME/nimble
+set -x NODE_REPL_HISTORY $XDG_DATA_HOME/node_repl_history
+
+set -x LESSHISTFILE $XDG_STATE_HOME/less/history
+
 source ~/.config/nnn/init # nnn
 source ~/.config/cheat/init # cheat
 source ~/.config/starship/init # starship
