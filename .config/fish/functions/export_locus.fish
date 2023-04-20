8c45d06c9c2ca7727540384b9b66dd5a9bfe1f1d
Hrishikesh Barman <oss@geekodour.org>
2023-04-20 13:33:11 +0530

    wipp

---
+++ .config/fish/functions/export_locus.fish
@@ -0,0 +1,12 @@
+function export_locus --description 'export most of locus'
+  pushd ~/locus
+  set -l repos_to_export blog diary mogoz o scrap todayi
+  for r in $repos_to_export;
+      pushd $r
+      echo "exporting repos ~/locus/$r"
+      make export
+      echo "done exporting repos ~/locus/$r"
+      popd
+  end
+  popd
+end
