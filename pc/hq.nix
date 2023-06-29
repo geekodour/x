945c2901c74f325cfbcf1c5faac4dcc9b5110636
Hrishikesh Barman <oss@geekodour.org>
2023-06-30 02:34:41 +0530

    67

--- pc/hq.nix
+++ pc/hq.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }:
 let
   home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz";
+  h = "/home/zuck";
   x = "/home/zuck/x";
 in
 {
@@ -13,43 +14,65 @@ in
     useGlobalPkgs = true;
     users.zuck = {pkgs, ... }: {
       home.stateVersion = "23.05";
-      programs.fish.enable = true; # maybe remove this later
 
       home.file.".config/git".source = "${x}/.config/git/";
+      home.file.".config/waybar".source = "${x}/.config/waybar/";
       home.file.".config/sway/config".source = "${x}/.config/sway/config_for_hq";
       home.file.".config/alacritty".source = "${x}/.config/alacritty";
-      #home.file.".tmux.conf".source = "/home/zuck/d/.tmux.conf";
+      home.file.".config/nvim".source = "${x}/.config/nvim";
+      home.file.".config/wofi".source = "${x}/.config/wofi";
+      home.file.".config/zoxide".source = "${x}/.config/zoxide";
+      home.file.".config/nnn/init".source = "${x}/.config/nnn/init";
+      # fish
+      programs.fish.enable = true; # maybe remove this later
+      home.file.".config/fish/functions".source = "${x}/.config/fish/functions";
+      home.file.".config/fish/conf.d/gitaliases.fish".source = "${x}/.config/fish/conf.d/gitaliases.fish";
+
 
       home.packages = with pkgs; [
-        tmux
-	sway
-	firefox
-        alacritty
-	fzf
+          tmux
+          sway
+          firefox
+          alacritty
+          fzf
+          zoxide
+          bat
+          nnn
 
-	# display
-	wl-clipboard
-	clipman
-	wofi
-	swayidle
-	swaylock
-	waybar
+          # display
+          wl-clipboard
+          clipman
+          wofi
+          wofi-emoji
+          swayidle
+          swaylock
+          waybar
 
-	# NOTE: Sway v/s Nvidia is a mess
-	# WLR_RENDERER=vulkan helps, so we need vulkan related packages
-        glslang
-        vulkan-headers
-        vulkan-loader
-        vulkan-validation-layers
-      ];
+          # NOTE: Sway v/s Nvidia is a mess
+          # WLR_RENDERER=vulkan helps, so we need vulkan related packages
+          glslang
+          vulkan-headers
+          vulkan-loader
+          vulkan-validation-layers
+          ];
       home.sessionVariables = {
-	XWAYLAND_NO_GLAMOR = "1";
-  	WLR_NO_HARDWARE_CURSORS = "1";
+        # sway
+        XWAYLAND_NO_GLAMOR = "1";
+        WLR_NO_HARDWARE_CURSORS = "1";
         LIBVA_DRIVER_NAME = "nvidia";
         XDG_SESSION_TYPE = "wayland";
         GBM_BACKEND = "nvidia-drm"; # remove if issue w ff
-        __GLX_VENDOR_LIBRARY_NAME = "nvidia"; # remove if discord issues
-	WLR_RENDERER = "vulkan";
+        __GLX_VENDOR_LIBRARY_NAME = "nvidia"; # remove if issues w discord
+        WLR_RENDERER = "vulkan";
+        # xdg
+        XDG_CACHE_HOME = "${h}/.cache";
+        XDG_CONFIG_HOME = "${h}/.config";
+        XDG_DATA_HOME = "${h}/.local/share";
+        XDG_STATE_HOME = "${h}/.local/state";
+        # misc
+        MANPAGER = "sh -c 'col -bx | bat -l man -p'";
+        DOOMDIR = "${h}/.config/doom";
+        EDITOR = "nvim";
       };
     };
   };
