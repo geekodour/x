a82ce76a2dc356a94822106d6c5016d04497635b
Hrishikesh Barman <oss@geekodour.org>
2023-06-30 01:21:19 +0530

    copied config

---
+++ pc/configuration_hq.nix
@@ -0,0 +1,93 @@
+# Help: configuration.nix(5), nixos-help
+
+{ config, pkgs, ... }:
+
+
+{
+  imports = [
+      ./hardware-configuration.nix
+      /home/zuck/x/pc/hq.nix
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+  services.xserver.videoDrivers = ["nvidia"]; # needed for wayland too
+  hardware.nvidia = {
+     modesetting.enable = true;
+     open = true; # open source kernel module 515.43.04+
+     nvidiaSettings = true;
+     package = config.boot.kernelPackages.nvidiaPackages.stable;
+   };
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "hq";
+  networking.nameservers = [ "1.1.1.1" "8.8.8.8" ];
+  networking.wireless.iwd.enable = true;  
+  networking.wireless.userControlled.enable = true;
+  security.polkit.enable = true;
+  security.pam.services.swaylock = {
+    text = "auth include login";
+  };
+
+  time.timeZone = "Asia/Kolkata";
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  users.users.zuck = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+    shell = pkgs.fish;
+  };
+
+  # system packages
+  programs.fish.enable = true; # for fish completion
+  environment.systemPackages = with pkgs; [
+      neovim
+      wget
+      pciutils
+      ripgrep
+      git
+      htop
+      iwd
+  ];
+
+  fonts.fonts = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk
+    noto-fonts-emoji
+    fira-code
+    fira-code-symbols
+    jetbrains-mono
+    (nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" ]; })
+  ];
+
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # Enabled services
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+
+  system.stateVersion = "23.05";
+}
+
