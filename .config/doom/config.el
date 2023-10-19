ea5d063f8f311ac17aeab13c7d5a9c0e2c1aacb8
Hrishikesh Barman <oss@geekodour.org>
2023-10-19 12:37:58 +0530

    sql formatter emacs

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -67,7 +67,12 @@
   :config
   ;; (apheleia-global-mode +1) ;; don't format by default/automatically
   (setf (alist-get 'python-mode apheleia-mode-alist)
-        '(isort black)))
+        '(isort black))
+  (setf (alist-get 'pg_format apheleia-formatters)
+        '("pg_format" "-"))
+  (setf (alist-get 'sql-mode apheleia-mode-alist)
+        '(pg_format))
+   )
 (push '(sh-mode . shfmt) apheleia-mode-alist) ; apheleia for some reason does not already do this
 
 (add-hook! prog-mode #'flymake-mode) ; start flymake-mode by default only for programming language
