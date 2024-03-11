1890350c31dd6a93898dfe1fd29f8586cb1cb425
Hrishikesh Barman <oss@geekodour.org>
2024-03-11 21:21:33 +0530

    wip

--- lander/lander.nix
+++ lander/lander.nix
@@ -21,6 +21,14 @@ in
   #  };
   #};
 
+  services.jellyfin.enable = true;
+  services.jellyfin.openFirewall = true;
+  environment.systemPackages = [
+    pkgs.jellyfin
+    pkgs.jellyfin-web
+    pkgs.jellyfin-ffmpeg
+  ];
+
   security.wrappers.pmount = {
     setuid = true;
     setgid = true;
