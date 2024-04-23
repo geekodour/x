21205185f3db49b3278ed5917ad3046a0607e195
Hrishikesh Barman <oss@geekodour.org>
2024-04-23 16:03:11 +0530

    wip

--- .config/fish/functions/r.fish
+++ .config/fish/functions/r.fish
@@ -3,8 +3,11 @@ function r --wraps=nnn --description 'alias r nnn'
   # -a auto setup fifo
   # -d detail mode
   # -o open files on return key only
-  # -r show cp/mv progress
   # -Q disable confirmation on quit w multple act. contexts
+
+  # NOTE: we're removing -r flag for this: https://github.com/NixOS/nixpkgs/issues/271508
+  # -r show cp/mv progress 
+
   #nnn -adorQ $argv; 
-  nnn -acdorQ $argv; 
+  nnn -acdoQ $argv; 
 end
